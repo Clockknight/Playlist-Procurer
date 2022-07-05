1423c5246c5d19245df7dbb90a072eb267aa8a73
Clockknight <tylerf2wang@gmail.com>
2022-07-04 23:06:55 -0700

    Made outline of plan with todo comments

---
+++ playlistProcurer.py
@@ -0,0 +1,7 @@
+import pytube
+
+
+# TODO take playlist url
+url = input("Please input the URL of the YouTube Playlist that you want to Procure.")
+# TODO pytube parse the playlist to get list of all the videos
+# TODO pytube parse each video as an mp3 file
