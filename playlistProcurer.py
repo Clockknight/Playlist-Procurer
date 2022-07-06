eff44145f91b353e23285587ad4e5d4c2df8c085
Clockknight <tylerf2wang@gmail.com>
2022-07-06 01:49:03 -0700

    Script works as intended

--- playlistProcurer.py
+++ playlistProcurer.py
@@ -1,7 +1,21 @@
-import pytube
+import shutil
+from pytube import *
+from moviepy.editor import *
 
+url = input("Please input the URL of the YouTube Playlist that you want to Procure.\n\t")
+userPlaylist = Playlist(url)
+pltitle = userPlaylist.title
+if not os.path.exists(pltitle):
+        os.makedirs(pltitle)
 
-# TODO take playlist url
-url = input("Please input the URL of the YouTube Playlist that you want to Procure.")
-# TODO pytube parse the playlist to get list of all the videos
-# TODO pytube parse each video as an mp3 file
+for video in userPlaylist.videos:
+        mediapath4 = video.streams.filter(mime_type="audio/mp4").order_by("abr").last().download()
+        clip = AudioFileClip(mediapath4)
+
+        mediapath3 = mediapath4[:-1] + "3"
+        filename = os.path.basename(mediapath3)
+        clip.write_audiofile(mediapath3, logger=None)
+
+        os.rename(mediapath3, ".\\" + pltitle + "\\"+filename)
+        os.remove(mediapath4)
+        print(mediapath4)
