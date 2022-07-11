459d02dd72783f7a5c8d749d83e91e38756576f4
Clockknight <tylerf2wang@gmail.com>
2022-07-10 21:17:22 -0700

    Project on hold

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from work import *
 
+# TODO Come back when Pyinstaller works for 3.10
+# Pyinstaller would work for 3.9 py, but intention is to get album downloader exported, which isn't possible when pyinstaller doesnt work with 3.10
 main()
